92b9cc16fcafee9c6c9c8b3e7b2fedf013801655
LENOVO-EET\LENOVO_EET <pawel.mirys@gmail.com>
2024-02-06 12:20:41 +0100

    Food card design

--- src/components/FetchedFoodList/FetchedFoodList.tsx
+++ src/components/FetchedFoodList/FetchedFoodList.tsx
@@ -12,7 +12,11 @@ const FetchedFoodList: React.FC<FetchedFoodListProps> = ({ data }) => {
     );
   });
 
-  return <div className='grid grid-cols-6 gap-10 mx-10 mt-5'>{list}</div>;
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-10 mx-10 mt-5'>
+      {list}
+    </div>
+  );
 };
 
 export default FetchedFoodList;
